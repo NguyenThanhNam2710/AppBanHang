ae288e20f968957ce78b1cd2e5d7a8fa834bc4c8
Nguyen Thanh Nam <namntn2710@gmail.com>
2020-09-18 14:20:55 +0700

    Initial Commit

---
+++ AppBanHang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBanHang
+//
+//  Created by Nguyen Thanh Nam on 9/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
